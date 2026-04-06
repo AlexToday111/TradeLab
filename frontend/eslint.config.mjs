aff0d52870dc47e0306754493cb1458e0fa52472
AlexToday111 <ernestkudakaev6@mail.ru>
2026-04-06 18:18:17 +0300

    fix(frontend): use next flat eslint config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,13 +1,12 @@
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
 
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-export default [...compat.extends("next/core-web-vitals")];
+export default defineConfig([
+  ...nextVitals,
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
